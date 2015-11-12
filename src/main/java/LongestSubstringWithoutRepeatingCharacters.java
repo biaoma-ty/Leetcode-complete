7c1a87aa97d21ea1f16254f96b11d24efdacee96
f7753 <mabiaocas@gmail.com>
2015-11-12 16:04:44 +0800

    Fisrst vcs test

---
+++ src/main/java/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,13 @@
+/**
+ * Created by INFI on 2015/11/12.
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+    public int lengthOfLongestSubstring(String s) {
+
+    }
+    public static void main(String[] args) {
+        LongestSubstringWithoutRepeatingCharacters test =
+                new LongestSubstringWithoutRepeatingCharacters();
+
+    }
+}
